faaaf7c0c6754d6fd781f2f668b15a33eee74e83
Estefan Hu <estefanhu074@gmail.com>
2019-12-03 10:13:51 -0800

    installed async funcitonalaityh

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 'use strict';
-const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const util = require('util');
+const glob = require('glob');
+const globPromise = util.promisify(glob);
+
+glob('raw/*.json', (err, matches) => {
+  if (err) {
+    console.log("ERROR: " + err);
+  } else {
+    for (let i = 0; i < matches.length; i++) {
+      (async () => {
+        let contents = await fs.readFile(matches[i], 'utf-8');
+        console.log(contents);
+      })();
+    }
+  }
+  console.log("----Script Complete----");
+});
