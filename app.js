74b70a756fb572c5a34ed1e7541de20abf5c6c84
Estefan Hu <estefanhu074@gmail.com>
2019-12-03 09:47:02 -0800

    removed unused packages

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
 'use strict';
-const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
-const app = express();
-
-
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
